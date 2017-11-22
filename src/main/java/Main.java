4ced2a8c58fff9648529e5126d8a8387a0e9b3bf
motohiroe shiobara <siobara.motohiro@gmail.com>
2017-11-22 10:18:20 +0900

    エラーの訂正

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import java.io.File;
+
 public class Main {
   public static void main(String[] args) {
     BitFlyer client = new BitFlyer();
     double price = client.fetchMidPrice();
     String strPrice = String.valueOf(price);
-    String homePath = System.getenv("HOME");
-    String filepath = homePath + "/projects/AppleScript/MacAlert.script";
+    String path = new File(".").getAbsoluteFile().getParent();
+    // ファイルパスの設定を各自で行う
+    String filepath = path + "/apple_script/mac_alert.script";
+
     try {
       Process process = new ProcessBuilder("osascript", filepath, strPrice).start();
     } catch(Exception ex) {
